ad0055565b0a1d79dc5d02c5a404b36ea1b0e46a
Lucas Silva <76401911+lucasDSBR@users.noreply.github.com>
2023-07-17 16:33:34 -0300

    Create query.cs

---
+++ query.cs
@@ -0,0 +1,38 @@
+using(var con = new SqlConnection(stringConnection))
+{
+  var sql = "SELECT * FROM Custumers";
+  var customers = con.Query(sql);
+  
+  foreach(var customer in customers)
+  {
+    Console.WriteLine($"{customer.CustomerID} {customer.CompanyName}");
+  }
+}
+
+//Realizando mapeamento
+public class Customer
+{
+    public string CustomerID { get; set; }
+    public string CompanyName { get; set; }
+    public string ContactName { get; set; }
+    public string ContactTitle { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string Region { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+    public string Phone { get; set; }
+    public string Fax { get; set; }
+}
+
+
+using (var connection = new SqlConnection(connectionString))
+{
+    var sql = "SELECT * FROM Customers";
+    var customers = connection.Query<Customer>(sql);
+	
+    foreach(var customer in customers)
+    {
+        Console.WriteLine($"{customer.CustomerID} {customer.CompanyName}");
+    }
+}
